e4a5c4533d8bb1bbd0d172e3239fd47bb76dc672
ConnorLear <connor.a.lear@gmail.com>
2024-10-01 21:36:37 +0000

    Update

--- primm_11.py
+++ primm_11.py
@@ -10,7 +10,7 @@ def main():
   #combines the two numbers the user puts in and prints them
     num1: int = input("Enter a number: ")
     num2: int = input("Enter another number: ")
-    total: int = int(num1)%int(num2)
+    total: int = int(num1)+int(num2)
 
     print(f"{num1} + {num2} = {total}")
 
